37b91d5187295004860d0e2df73fdd098fda5519
Boruto2001 <vishaalshekar2001@gmail.com>
2022-05-30 21:10:43 +0530

    Photos9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+ baseUrl = environment.apiUrl;
+//  baseUrl='https://localhost:5001/api/';
   private currentUserSource = new ReplaySubject<User>(1); //user object buffer
   currentUser$ = this.currentUserSource.asObservable();
 
